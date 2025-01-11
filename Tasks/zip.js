fed426508f473ab69739e5b2879588b2ba9f726b
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-12 00:02:34 +0200

    Third: add intermediate variables

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -2,6 +2,8 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 'use strict';
 const zip = (a, b) => {
+  const zipArr = [];
+  const size = Math.min(a.length, b.length);
   let i = 0;
   let j = 0;
   for (const x of b) {
