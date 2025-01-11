7d7c9b8761f198df3b7e44cbcb6cbc0f4de6b0f8
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-12 00:12:01 +0200

    Sixth: fix some naming

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,11 +1,11 @@
 // Refactor following solution
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 'use strict';
-const zip = (a, b) => {
+const zip = (fistArr, secondArr) => {
   const zipArr = [];
-  const size = Math.min(a.length, b.length);
+  const size = Math.min(fistArr.length, secondArr.length);
   for (let i = 0; i < size; i++) {
-    zipArr[i] = [a[i], b[i]];
+    zipArr[i] = [fistArr[i], secondArr[i]];
   }
   return zipArr;
 };
