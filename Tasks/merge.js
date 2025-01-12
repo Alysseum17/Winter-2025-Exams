35e20402ff291af63c959d68a107b7ea03e7e65c
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-12 16:52:22 +0200

    Third: remove useless code and unnecessary check

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -2,16 +2,14 @@
 // Merge two dictionaries
 'use strict';
 const mergeTwoObjects = (objectOne, objectTwo) => {
-  const mergedObject = [{}][0];
+  const mergedObject = {};
   for (const keyOne in objectOne) {
-    mergedObject[keyOne] == objectOne[keyOne];
     mergedObject[keyOne] = objectOne[keyOne];
   }
   for (const keyTwo in objectTwo) {
-    mergedObject[keyTwo] == objectTwo[keyTwo];
     mergedObject[keyTwo] = objectTwo[keyTwo];
   }
-  return objectOne, objectTwo, mergedObject;
+  return mergedObject;
 };
 
 module.exports = mergeTwoObjects;
