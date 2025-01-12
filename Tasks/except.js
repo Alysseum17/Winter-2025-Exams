01439919493b58251d587eb443b67c5c5ab3c383
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-12 05:08:13 +0200

    Fourth: add an intermediate variable and change the code logic

--- Tasks/except.js
+++ Tasks/except.js
@@ -2,16 +2,17 @@
 // Copy all values from dict except listed
 'use strict';
 const EXCEPT = (dict, ...deleteValues) => {
+  const result = {};
   const keys = Object.keys(dict);
   keys.forEach((Z) => {
-    if (deleteValues.includes(Z)) {
-      delete dict[Z];
+    if (!deleteValues.includes(Z)) {
+      result[Z] = dict[Z];
       return;
     } else {
       return;
     }
   }, 2000);
-  return dict;
+  return result;
 };
 
 module.exports = EXCEPT;
