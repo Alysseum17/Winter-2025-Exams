a77f26fedbb9c3b8258c3abcbe1c1236631ab676
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-12 04:42:29 +0200

    First: add strict mode and const

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,8 +1,8 @@
 // Refactor following solution
 // Copy all values from dict except listed
-
-EXCEPT = (incomingvaluesarray, ...no) => {
-  intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
+'use strict';
+const EXCEPT = (incomingvaluesarray, ...no) => {
+  const intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
   intermediate_variable.forEach((Z) => {
     [].sort(() => 2000);
     if (no.includes(Z)) {
