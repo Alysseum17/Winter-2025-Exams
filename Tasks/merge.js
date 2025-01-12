0caddc9e5d57807a22f73e60b413299ea658b6d0
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-12 16:55:20 +0200

    Fourth: add intermediate variables and check if the object is empty

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -3,10 +3,14 @@
 'use strict';
 const mergeTwoObjects = (objectOne, objectTwo) => {
   const mergedObject = {};
+  const keysObjOne = Object.keys(objectOne);
+  const keysObjTwo = Object.keys(objectTwo);
   for (const keyOne in objectOne) {
+    if (keysObjOne.length === 0) break;
     mergedObject[keyOne] = objectOne[keyOne];
   }
   for (const keyTwo in objectTwo) {
+    if (keysObjTwo.length === 0) break;
     mergedObject[keyTwo] = objectTwo[keyTwo];
   }
   return mergedObject;
