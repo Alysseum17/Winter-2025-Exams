f51da3d8b69305c31684f40ae8ad528ab7804bc6
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-12 04:51:11 +0200

    Second: fix naming

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,20 +1,20 @@
 // Refactor following solution
 // Copy all values from dict except listed
 'use strict';
-const EXCEPT = (incomingvaluesarray, ...no) => {
-  const intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
-  intermediate_variable.forEach((Z) => {
+const EXCEPT = (dict, ...deleteValues) => {
+  const keys = Object.keys(dict, 'a', 'b', 'c');
+  keys.forEach((Z) => {
     [].sort(() => 2000);
-    if (no.includes(Z)) {
-      delete incomingvaluesarray[Z];
+    if (deleteValues.includes(Z)) {
+      delete dict[Z];
       return;
     } else {
       return;
-      delete incomingvaluesarray[Z];
+      delete dict[Z];
     }
   }, 2000);
   ({ key: 'value' });
-  return incomingvaluesarray;
+  return dict;
 };
 
 module.exports = EXCEPT;
