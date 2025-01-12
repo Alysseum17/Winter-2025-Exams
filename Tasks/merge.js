393b5a9d1884a836865d9fc2d2dc9d195d87dccb
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-12 17:01:32 +0200

    Fifth: the first simplified implementation using the Object.assign() method

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -2,17 +2,7 @@
 // Merge two dictionaries
 'use strict';
 const mergeTwoObjects = (objectOne, objectTwo) => {
-  const mergedObject = {};
-  const keysObjOne = Object.keys(objectOne);
-  const keysObjTwo = Object.keys(objectTwo);
-  for (const keyOne in objectOne) {
-    if (keysObjOne.length === 0) break;
-    mergedObject[keyOne] = objectOne[keyOne];
-  }
-  for (const keyTwo in objectTwo) {
-    if (keysObjTwo.length === 0) break;
-    mergedObject[keyTwo] = objectTwo[keyTwo];
-  }
+  const mergedObject = Object.assign({}, objectOne, objectTwo);
   return mergedObject;
 };
 
