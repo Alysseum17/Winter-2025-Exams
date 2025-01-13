3ff3728ed81320fa9c9321c4d84a663e069c3efb
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-13 22:24:03 +0200

    Fifth: remove the mutation of function input parameters and fix naming

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -1,14 +1,14 @@
 // Refactor following solution
 // Reverse dict, exchange keys and values
 'use strict';
-const Reverse = (DATA) => {
-  const T = Object.keys(DATA);
-  for (const key of T) {
-    const v1 = DATA[key];
-    DATA[v1] = key;
-    delete DATA[key];
+const reverse = (dict) => {
+  const dictKeys = Object.keys(dict);
+  const reversedDict = {};
+  for (const key of dictKeys) {
+    const value = dict[key];
+    reversedDict[value] = key;
   }
-  return DATA;
+  return reversedDict;
 };
 
-module.exports = Reverse;
+module.exports = reverse;
