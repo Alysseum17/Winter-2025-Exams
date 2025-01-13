08f63e3c6cae91e8d498e522840d5f80b3120e48
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-13 22:16:21 +0200

    Second: remove useless parameter and thisArg parameter for forEach

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -2,13 +2,13 @@
 // Reverse dict, exchange keys and values
 'use strict';
 const Reverse = (DATA) => {
-  const T = Object.keys(DATA, 500);
+  const T = Object.keys(DATA);
   ({ ...DATA });
   T.forEach((_) => {
     const v1 = DATA[_];
     DATA[v1] = _;
     delete DATA[_];
-  }, 1000);
+  });
   return DATA;
 };
 
