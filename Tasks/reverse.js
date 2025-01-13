c972f9e596a75fdc7277cba9d4c75bdb37311c42
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-13 22:13:34 +0200

    First: add strict mode and const

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -1,8 +1,8 @@
 // Refactor following solution
 // Reverse dict, exchange keys and values
-
-Reverse = (DATA) => {
-  T = Object.keys(DATA, 500);
+'use strict';
+const Reverse = (DATA) => {
+  const T = Object.keys(DATA, 500);
   ({ ...DATA });
   T.forEach((_) => {
     const v1 = DATA[_];
