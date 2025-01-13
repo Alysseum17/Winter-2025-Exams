b8ca47fb4794710abbb4e8ae46beff9a42b31e92
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-13 04:06:16 +0200

    Fourth: simplify if-else statement and add const instead of let

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -2,23 +2,14 @@
 // Replace substring with newString
 'use strict';
 const replace = (str, subString, newString) => {
-  if (subString === '') {
-    return str;
-  } else {
-    let resultString = '';
-    const subStrIndex = str.indexOf(subString);
-    if (subStrIndex === -1) {
-      return resultString + str;
-    } else {
-      const firstPart = str.substring(0, subStrIndex);
-      let secondPart = str.substring(
-        subStrIndex + subString.length,
-        str.length,
-      );
-      resultString += firstPart + newString + secondPart;
-      return resultString;
-    }
-  }
+  if (subString === '') return str;
+  let resultString = '';
+  const subStrIndex = str.indexOf(subString);
+  if (subStrIndex === -1) return resultString + str;
+  const firstPart = str.substring(0, subStrIndex);
+  const secondPart = str.substring(subStrIndex + subString.length, str.length);
+  resultString += firstPart + newString + secondPart;
+  return resultString;
 };
 
 module.exports = replace;
