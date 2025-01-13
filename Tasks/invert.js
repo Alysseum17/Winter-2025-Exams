f857887ac81ffec5a6965f850ab46556bf9d22d3
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-13 21:59:42 +0200

    Sixth: fix naming

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,12 +1,12 @@
 // Refactor following solution
 // Reverse an array, you can't use .reverse()
 'use strict';
-const invert = (A) => {
-  const T = Object.keys(A);
-  for (const key of T) {
-    T[key] = A[A.length - key - 1];
+const invert = (arr) => {
+  const invertedArr = Object.keys(arr);
+  for (const key of invertedArr) {
+    invertedArr[key] = arr[arr.length - key - 1];
   }
-  return T;
+  return invertedArr;
 };
 
 module.exports = invert;
