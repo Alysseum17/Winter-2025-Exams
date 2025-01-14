7613082aa2bf1e42dc95502ead8a3f64888c6739
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-14 17:18:25 +0200

    Seventh: use const instead of let

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -2,7 +2,7 @@
 // Sum all numbers from an array
 'use strict';
 const sum = (arr) => {
-  let total = arr.reduce(
+  const total = arr.reduce(
     (acc, curValue) => (typeof curValue === 'number' ? acc + curValue : acc),
     0,
   );
