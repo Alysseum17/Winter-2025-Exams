9b13702bcd8f2ffcca66e1420d19aa29701e8cd9
Dania <marchenko.daniil@lll.kpi.ua>
2025-01-14 17:29:10 +0200

    Seventh: do in one line

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,9 +1,6 @@
 // Refactor following solution
 // Filter array by type name
 'use strict';
-const filter = (arr, type) => {
-  const filteredArr = arr.filter((value) => typeof value === type);
-  return filteredArr;
-};
+const filter = (arr, type) => arr.filter((value) => typeof value === type);
 
 module.exports = filter;
